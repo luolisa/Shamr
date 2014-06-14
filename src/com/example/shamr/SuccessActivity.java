e51933c318072fe3ac3cd03b3cb49ff5ac508141
Scott Rogers <srogers@amplify.com>
2014-06-14 14:05:40 -0400

    add launch activity

--- src/com/example/shamr/SuccessActivity.java
+++ src/com/example/shamr/SuccessActivity.java
@@ -1,16 +1,7 @@
 package com.example.shamr;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
 public class SuccessActivity extends Activity {
 
@@ -20,27 +11,5 @@ public class SuccessActivity extends Activity {
 		setContentView(R.layout.activity_success);
 
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.success, menu);
-		
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
 
 }
